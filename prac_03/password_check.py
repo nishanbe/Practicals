f94246637f7c43dac6a3e83e9cec71e5d1513fae
nishanbe <Msjammsjam1!>
2021-03-15 01:43:09 +1100

    Use Functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,12 +1,24 @@
 MIN_PASSWORD_LENGTH = 4
-# write a program that asks the user for a password
-password = input("Enter a password: ")
 
-# with error-checking to repeat if the password doesn't meet a minimum length set by a variable.
-while len(password) < MIN_PASSWORD_LENGTH:
-    print("The password you entered is too short, enter at least", MIN_LENGTH, "characters.")
+
+def main():
+    password = get_password(MIN_PASSWORD_LENGTH)
+    print_asterisk_per_character(password)
+
+
+def get_password(min_length):
+    # write a program that asks the user for a password
     password = input("Enter a password: ")
-else:
-    # The program should then print asterisks as long as the word
-    for char in password:
+    # with error-checking to repeat if the password doesn't meet a minimum length set by a variable.
+    while len(password) < min_length:
+        print("The password you entered is too short, enter at least", min_length, "characters.")
+        password = input("Enter a password: ")
+    return password
+
+
+def print_asterisk_per_character(count):
+    for char in count:
         print("*", end="")
+
+
+main()
