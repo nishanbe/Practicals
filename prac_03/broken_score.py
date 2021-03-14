19d674ddc78d5078a873b2253f10431dab8dc73f
nishanbe <Msjammsjam1!>
2021-03-15 02:50:46 +1100

    Use Functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,18 +2,42 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+# main should ask the user for their score and print the result.
+# write a new function that takes in the user's score as a parameter and returns the result to be printed.
+# The function should not print it.
+import random
 
-# TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
-        print("Invalid score")
-    elif (score >= 50) and (score < 90):
-        print("Passable")
-    elif score >= 90:
-        print("Excellent")
-    elif score < 50:
-        print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    print(assess(score))
+    print("-----------------")
+    randomise_scores()
+
+
+def assess(score):
+    result = ""
+    if score < 0:
+        result = "Invalid score"
+    else:
+        if score > 100:
+            result = "Invalid score"
+        elif score >= 90:
+            result = "Excellent"
+        elif score >= 50:
+            result = "Passable"
+        elif score < 50:
+            result = "Bad"
+
+    return result
+
+
+def randomise_scores():
+    random_count = int(input("Enter the amount of random scores:"))
+    print("Here are {:d} random scores:".format(random_count))
+    for i in range(random_count):
+        score = random.randint(1, 100)
+        print("{} - The score {} is {}".format(i+1, score, assess(score)))
+
+
+main()
