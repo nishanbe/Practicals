ded280a6891ea539c0db9cfadf579bb29fe9c596
nishanbe <79426795+nishanbe@users.noreply.github.com>
2021-03-14 18:19:31 +1100

    Delete test directory

--- test/w3.py
+++
@@ -1,9 +0,0 @@
-number = int(input("Give me a number to square it:"))
-
-
-def square_it(num):
-    num = num * num
-    return num
-
-
-print("The square if", number, "is", square_it(number))
--- test/write.py
+++
@@ -1,5 +0,0 @@
-
-out_file = open("Nishan.txt", "a")
-print("78" , file=out_file , end="")
-out_file.write("23\n")
-out_file.close()
