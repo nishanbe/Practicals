4349ea620c98fd509c7266891f984c1a316365f6
nishanbe <Msjammsjam1!>
2021-03-14 19:08:32 +1100

    Initial Commit

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,9 +1,9 @@
-MIN_LENGTH = 4
+MIN_PASSWORD_LENGTH = 4
 # write a program that asks the user for a password
 password = input("Enter a password: ")
 
 # with error-checking to repeat if the password doesn't meet a minimum length set by a variable.
-while len(password) < MIN_LENGTH:
+while len(password) < MIN_PASSWORD_LENGTH:
     print("The password you entered is too short, enter at least", MIN_LENGTH, "characters.")
     password = input("Enter a password: ")
 else:
