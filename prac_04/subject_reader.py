d721bea09812f1730308c3622d3a499d511f17e5
nishanbe <Msjammsjam1!>
2021-03-25 03:00:39 +1100

    Process information from a text file and display them nicely

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,31 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    display_nicely(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
+    input_file = open(FILENAME, "r")
+    all_parts = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
+        # print("This is a line", line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
+        line = line.strip("\n")  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+        all_parts.append(parts)
+
     input_file.close()
+    return all_parts
+
+
+def display_nicely(all_parts):
+    for part in all_parts:
+        print(part[0], "is taught by", part[1], "and has", part[2], "students")
+# CP1401 is taught by Ada Lovelace and has 192 students
 
 
 main()
