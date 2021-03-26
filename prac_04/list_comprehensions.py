69ae2d6640eeab62a1070adcf4ac46e149876df5
nishanbe <Msjammsjam1!>
2021-03-26 12:03:19 +1100

    Completes all of the To Do items

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -19,8 +19,7 @@ print(first_initials)
 
 # list comprehension that creates a list containing the initials
 # splits each name and adds the first letters of each part to a string
-full_initials = [name.split()[0][0] + name.split()[1][0] for name in
-                 full_names]
+full_initials = [name.split()[0][0] + name.split()[1][0] for name in full_names]
 print(full_initials)
 
 # one more example, using filtering to select only the names that start with A
@@ -29,12 +28,16 @@ print(a_names)
 
 # TODO: use a list comprehension to create a list of all of the full_names
 # in lowercase format
-# lowercase_full_names =
+lowercase_full_names = [name.lower() for name in names]
+print(lowercase_full_names)
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # TODO: use a list comprehension to create a list of integers
 # from the above list of strings
-# numbers =
+numbers = [int(number) for number in almost_numbers]
+print(numbers)
 
 # TODO: use a list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
+over_nine_numbers = [number for number in numbers if number > 9]
+print(over_nine_numbers)
