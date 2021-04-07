1d39996bea1ceb9f32c2a750bf4febc4c2391ba7
nishanbe <Msjammsjam1!>
2021-04-07 22:30:13 +1000

    Improve the CONSTANT name Add a the main function to display user instructions.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,6 +3,17 @@ CP1404/CP5632 Practical
 Intermediate Exercises
 """
 
-COLOURS = {"red1": "#ff0000", "green1": "#00ff00", "blue1": "#0000ff",
-           "yellow1": "#ffff00", "cyan1": "#00ffff", "magenta": "#ff00ff",
-           "gray6": "#0f0f0f", "honeydew1": "#f0fff0", "white": "#ffffff", "black": "#000000"}
+NAME_TO_COLOUR = {"red1": "#ff0000", "green1": "#00ff00", "blue1": "#0000ff",
+                  "yellow1": "#ffff00", "cyan1": "#00ffff", "magenta": "#ff00ff",
+                  "gray6": "#0f0f0f", "honeydew1": "#f0fff0", "white": "#ffffff",
+                  "black": "#000000"}
+
+
+def main():
+    """This program allows you to get the hex code of colour names you enter"""
+    print("Enter any of the colour names below to get its hex code:")
+    print(*NAME_TO_COLOUR.keys(), sep=", ")
+
+
+if __name__ == '__main__':
+    main()
