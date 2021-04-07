242192cbf378b58a76f6e5da1f79de3285dd89ea
nishanbe <Msjammsjam1!>
2021-04-07 23:22:39 +1000

    Add a function to validate and respond to user's entry.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -12,12 +12,21 @@ NAME_TO_COLOUR = {"red1": "#ff0000", "green1": "#00ff00", "blue1": "#0000ff",
 def main():
     """This program allows you to get the hex code of colour names you enter"""
     print("Enter any of the colour names below to get its hex code:")
-    print(*NAME_TO_COLOUR.keys(), sep=", ")
-    colour_name = input("Enter a colour name: ").upper()
+    print(*NAME_TO_COLOUR.keys(), sep=", ", end="\n\n")
+    colour_name = input("Enter a colour name: ").lower()
     while colour_name != "":
-        colour_name = input("Enter a colour name: ").upper()
+        validate_name(colour_name)
+        colour_name = input("Enter a colour name: ").lower()
+    print("Thanks for using this program. Hold tight for the next version with more colours!")
 
-    print("Thanks for using this program. Hold tight for the next version!")
+
+def validate_name(colour_name):
+    """Validate the colour name and respond"""
+    if colour_name in NAME_TO_COLOUR:
+        print(NAME_TO_COLOUR[colour_name], end="\n\n")
+    else:
+        print("\nInvalid name, please type one of the following colour names:")
+        print(*NAME_TO_COLOUR.keys(), sep=", ", end="\n\n")
 
 
 if __name__ == '__main__':
