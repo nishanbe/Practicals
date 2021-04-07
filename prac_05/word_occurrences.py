0110737db909d26b6ae31041a431e91170b10927
nishanbe <Msjammsjam1!>
2021-04-08 03:02:40 +1000

    Get text from the user.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,3 +2,15 @@
 CP1404/CP5632 Practical
 Count Word Occurrences
 """
+
+
+def main():
+    """This program counts the occurrences of each word in any sentence you enter"""
+    print("Type a sentence to get the count of each word.")
+    # Get text from the user
+    text = input("Text: ")
+    print("You entered:", text)
+
+
+if __name__ == '__main__':
+    main()
