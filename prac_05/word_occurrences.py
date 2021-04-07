3c2d7ffcb1cc928f1c26818540df8245ddd1706c
nishanbe <Msjammsjam1!>
2021-04-08 03:07:02 +1000

    Find the width of the longest word

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -15,6 +15,17 @@ def main():
     words = text.split()
     words.sort()
 
+    # Create a dictionary out from the list with each word and its count
+    words_dict = {}
+    for word in words:
+        words_dict[word] = words_dict.get(word, 0) + 1
+
+    # Find the width of the longest word
+    word_lengths = []
+    for word in words:
+        word_lengths.append(len(word))
+    longest_word_width = max(word_lengths)
+
 
 if __name__ == '__main__':
     main()
