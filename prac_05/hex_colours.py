76f5822b5d10561d9aecf5368e64a96b7095b6ce
nishanbe <Msjammsjam1!>
2021-04-07 22:13:43 +1000

    Add dictionary if colours

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,3 +2,7 @@
 CP1404/CP5632 Practical
 Intermediate Exercises
 """
+
+COLOURS = {"red1": "#ff0000", "green1": "#00ff00", "blue1": "#0000ff",
+           "yellow1": "#ffff00", "cyan1": "#00ffff", "magenta": "#ff00ff",
+           "gray6": "#0f0f0f", "honeydew1": "#f0fff0", "white": "#ffffff", "black": "#000000"}
