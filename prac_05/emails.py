765de4b3e91b09b4a573cec797fd042efa9728f8
nishanbe <Msjammsjam1!>
2021-04-08 13:04:06 +1000

    Add a function to validate predicted name and re-prompt if inaccurate.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -7,7 +7,10 @@ Email Register
 def main():
     """This program allows you to store your email, predict and confirm your full name."""
     email = input("Email: ")
-    print(extract_full_name(email))
+    predicted_full_name = extract_full_name(email)
+    confirmed_name = input("Is your name {}? (Y/n) ".format(predicted_full_name)).upper()
+    name = validate_name(predicted_full_name, confirmed_name)
+    print(name)
 
 
 def extract_full_name(email):
@@ -16,6 +19,16 @@ def extract_full_name(email):
     extracted_name = extracted_name.split(".")
     extracted_name = " ".join(extracted_name).title()
     return extracted_name
+
+
+def validate_name(predicted_full_name, confirmed_name):
+    """Validate predicted name and re-prompt if inaccurate"""
+    if confirmed_name == "Y":
+        name = predicted_full_name
+    else:
+        name = input("Name: ")
+        name = name.title()
+    return name
 
 
 if __name__ == '__main__':
