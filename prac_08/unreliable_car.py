4310f588d1f46dc0d8105ae2690be62915797852
nishanbe <Msjammsjam1!>
2021-05-01 19:08:24 +1000

    Complete

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -9,18 +9,18 @@ class UnreliableCar(Car):
     def __str__(self):
         return "{}, {}".format(self.name, self.reliability)
 
-    def drive(self, distance, reliability):
+    def drive(self, distance):
         """Only drive the car if the breakdown chance is less than the car's reliability"""
 
         import random
         breakdown_chance = float(random.randint(0, 100))
         if breakdown_chance < self.reliability:
-            driven_distance = super().drive(distance)
-            print("WARNING:\nWith {}% reliability the chance of "
-                  "a breakdown driving {}KMs is {}%".format(self.reliability,
-                                                            driven_distance,
-                                                            breakdown_chance))
+            # driven_distance = super().drive(distance)
+            # print("WARNING:\nWith {}% reliability the chance of "
+            #       "a breakdown driving {}KMs is {}%".format(self.reliability,
+            #                                                 driven_distance,
+            #                                                 breakdown_chance))
             distance = 0
         driven_distance = super().drive(distance)
-        print("The distance driver is: ", distance)
+        # print("The distance driver is: ", distance)
         return driven_distance
--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -1,5 +1,5 @@
 from prac_08.unreliable_car import UnreliableCar
 
-volov = UnreliableCar("Volvo", 100, 30.3)
-print(volov)
-print(volov.drive(40, 50))
+volvo = UnreliableCar("Volvo", 100, 30.3)
+print(volvo)
+print(volvo.drive(50))
