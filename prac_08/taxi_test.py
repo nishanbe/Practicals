9a1a80babdcfed35e483e4ce97872374aea4f099
nishanbe <Msjammsjam1!>
2021-05-01 13:35:16 +1000

    Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -1,1 +1,12 @@
 """Test Taxi"""
+from prac_08.taxi import Taxi
+
+"""
+Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km
+Drive the taxi 40km
+Print the taxi's details and the current fare
+Restart the meter (start a new fare) and then drive the car 100km
+Print the details and the current fare
+"""
+
+taxi1 = Taxi("Prius 1", 100, 1.23)
